322dc450089a8603778cbfb46ec912fc2465b79e
Rafael Jaques <rafael@phpit.com.br>
2012-07-31 16:01:05 -0300

    Código simplificado e melhorado com a ajuda do haesbaert

--- sorteio.py
+++ sorteio.py
@@ -6,20 +6,15 @@
 # ------------------------------------------------------------------------- #
 # ------------------------------------------------------------------------- #
 
-import random, math, os
-
-# Limpa a tela
-os.system('clear')
-
-# Números que já foram sorteados
-jaSorteados = []
+import random
+import math
+import os
 
 # Nome do pessoal que vai participar do sorteio
 galera = [
 			'Fulaninho',
 			'Ciclaninho',
 			'Pedrinho',
-			
 			# Coloque seu nome aqui! :)
 		 ]
 
@@ -27,38 +22,33 @@ galera = [
 # ---------- Início da Rotina de Sorteio de Brindes do TcheLinux ---------- #
 # ------------------------------------------------------------------------- #
 
+# Limpa a tela
+os.system('clear')
+
 print "Seja bem vindo ao sorteio de brindes do TcheLinux"
 
 # Verifica nomes duplicados
-
 if (len(galera) != len(set(galera))):
 	print("\n-> ERRO! Algum(a) safadinho(a) se cadastrou duas vezes!\n")
 	exit()
-	
 
 print "Uma vez sorteado, seu nome não será mostrado novamente.\n"
 print "Existem "+str(len(galera))+" pessoas na lista do sorteio! Bora lá?\n"
 
-while True:
+while galera:
 	# Prompt para sorteio
-	vaiMais = raw_input("Sortear mais um [s/n]? ")
+	sortearMais = raw_input("Sortear mais um [s/n]? ")
 	
-	if vaiMais == "s":
-		# Ao esvaziar a lista, encerra o programa
-		if len(galera) < 1:
-			print("\nVixi! Não existem mais pessoas para sortear!\n")
-			break
-		
-		# Sorteia um número entre 0 e len() da lista -1
-		numeroDaSorte = random.randint(0, len(galera)-1)
-		
+	if sortearMais == "s":
 		# Nome do sorteado
-		print("-> E a pessoa sortuda é: "+galera[numeroDaSorte]+"\n")
+		sorteado = random.choice(galera)
+		print("\n-> E a pessoa sortuda é: "+sorteado+"\n")
 		
-		# Remove o nome do sorteado da lista
-		jaSorteados.append(galera.pop(numeroDaSorte))
+		# Tira da lista
+		galera.remove(sorteado)
 		
 	else:
-		break
+		exit()
 		
-print "\nFim do programa\n"
+# Se ao sair do loop a lista estiver vazia, avisa que não existe mais o que sortear
+print("Todas as pessoas já foram sorteadas.")
